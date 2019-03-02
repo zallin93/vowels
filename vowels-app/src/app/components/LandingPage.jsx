e62e2aaff2051ba55e3c212078172799fadd02fb
Zachary Allin <zachary.allin@gartner.com>
2019-03-01 20:31:18 -0500

    package structure for styles, components

---
+++ vowels-app/src/app/components/LandingPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class LandingPage extends React.Component {
+
+    render() {
+        return (
+            <div>
+                Upload your file here!
+            </div>
+        );
+        
+    }
+};
+
+export default LandingPage;
