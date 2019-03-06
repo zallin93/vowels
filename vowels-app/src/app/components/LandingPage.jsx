73da39b3a1808157dd4be30659d9a452c195259b
Zachary Allin <zachary.allin@gartner.com>
2019-03-06 13:18:08 -0500

    switch to dumb component. put text in a span

--- vowels-app/src/app/components/LandingPage.jsx
+++ vowels-app/src/app/components/LandingPage.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import FileUploader from '../../app/components/FileUploader';
 
-class LandingPage extends React.Component {
-
-    render() {
-        return (
-            <div>
-                Upload your file here, then see the vowel counts in the Console!
-                <FileUploader />
-            </div>
-        );
-        
-    }
+const LandingPage = (props) => {
+    return (
+        <div>
+            <span>Upload your file here, then see the vowel counts in the Console!</span>
+            <FileUploader />
+        </div>
+    );
 };
 
 export default LandingPage;
