72f24df1d3d52bc1a10dde3ee7c682190752bebf
Zachary Allin <zachary.allin@gartner.com>
2019-03-06 22:04:18 -0500

    comments and formatting. "

--- vowels-util/index.js
+++ vowels-util/index.js
@@ -1,10 +1,16 @@
 const {VOWEL_REGEX} = require('./config.js');
 
+/**
+ * Main function, returns the number of occurences of vowels in all
+ * substrings of the input. 
+ *
+ * Complexity is O(n^2), aka not so good for larger inputs, 
+ * like `inputs/large.txt`.
+ */
 function countVowels(line) {
     let counter = 0;
 
     for(let i=0; i < line.length; i++) {
-
         for(let j=i; j <= line.length; j++) {
             counter += count(line.substring(i,j));        
         }
