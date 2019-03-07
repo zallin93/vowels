06b1ad7f5c117b9e823eb17e1fdf159daa1d7f6d
Zachary Allin <zachary.allin@gartner.com>
2019-03-06 23:11:38 -0500

    comments updated

--- vowels-util/index.js
+++ vowels-util/index.js
@@ -5,7 +5,11 @@ const {VOWEL_REGEX} = require('./config.js');
  * substrings of the input. 
  *
  * Complexity is O(n^2), aka not so good for larger inputs, 
- * like `inputs/large.txt`.
+ * like `inputs/large.txt`.  There may be a better methodology. 
+ *
+ * Next step would be to put a restriction on it, likely based on character 
+ * length of input.  Example test file suggests reasonable upper bound is below
+ * 10,000. 
  */
 function countVowels(line) {
     let counter = 0;
