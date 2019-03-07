b09995efe83ee3c218486f7a252ccbfc169c7ed5
Zachary Allin <zachary.allin@gartner.com>
2019-03-06 22:56:08 -0500

    reset state

--- vowels-app/src/app/components/LandingPage.jsx
+++ vowels-app/src/app/components/LandingPage.jsx
@@ -3,20 +3,22 @@ import FileUploader from '../../app/components/FileUploader';
 import Results from '../../app/components/Results';
 import {countVowels} from 'vowels-util';
 
-const initialState = {
-    resultCounts: [], 
-    hasSubmitted: false
-};
 
 class LandingPage extends React.Component {
     constructor(props) {
         super(props);
         this.handleUploadFunc = this.handleUploadFunc.bind(this);
 
-        this.state = initialState;
+        this.state = {
+            resultCounts: [], 
+            hasSubmitted: false
+        };
     }
 
     handleUploadFunc(file) {
+        this.setState({
+            hasSubmitted: true
+        });
         let reader = new FileReader();
         let self = this;
         reader.onload = (e) => {
@@ -46,7 +48,11 @@ class LandingPage extends React.Component {
                     isSubmitEnabled={!this.state.hasSubmitted} />
                 <Results resultCounts={this.state.resultCounts} 
                     showResults={this.state.hasSubmitted} 
-                    handleResetFunc={() => {this.setState({...initialState});}} />
+                    handleResetFunc={() => {
+                        this.setState({
+                            resultCounts: [],hasSubmitted: false
+                        });
+                    }} />
             </div>
         );
     }
